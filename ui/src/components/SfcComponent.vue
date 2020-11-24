c484b4b26272249c242f9a69268524f75368566c
Sonoko <sonoko@sourcedigital.net>
2020-11-24 15:32:14 -0500

    add: SFC example

---
+++ ui/src/components/SfcComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    This is Single File Component
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SfcComponent'
+}
+</script>
+
+<style>
+</style>
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,12 +1,14 @@
 import { version } from '../package.json'
 
 import Component from './components/Component'
+import SfcComponent from './components/SfcComponent.vue'
 
 
 export {
   version,
 
-  Component
+  Component,
+  SfcComponent
   
 }
 
@@ -14,10 +16,12 @@ export default {
   version,
 
   Component,
+  SfcComponent,
   
 
   install (Vue) {
     Vue.component(Component.name, Component)
+    Vue.component(SfcComponent.name, SfcComponent)
     
   }
 }
